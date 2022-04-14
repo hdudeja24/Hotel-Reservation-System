b3254e2598b9589fe062aefa5281809c019ae6e4
Hitanshu Dudeja <hitanshududeja80@gmail.com>
2022-04-14 02:44:20 -0400

    Test 2

--- Hotel-Reservation-System/Program.cs
+++ Hotel-Reservation-System/Program.cs
@@ -6,7 +6,7 @@ namespace Hotel_Reservation_System
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!, This is a test");
+            Console.WriteLine("Hello World!, This is a test 2");
         }
     }
 }
