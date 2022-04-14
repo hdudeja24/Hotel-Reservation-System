96ea21b09f0c3b7d6b8f92b1e41e0ed7ac890a53
Hitanshu Dudeja <hitanshududeja80@gmail.com>
2022-04-14 02:31:00 -0400

    Created the project

---
+++ Hotel-Reservation-System/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hotel_Reservation_System
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
